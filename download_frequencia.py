58878c5fec4cb34d3bf5c6cab8971657d4d74cd9
AfonsJr <lafonsojr@gmail.com>
2020-06-20 10:07:02 -0300

    Alterado o import do sleep url 2 variavel alterado para xpath a matricula e senha

--- download_frequencia.py
+++ download_frequencia.py
@@ -1,37 +1,39 @@
 from selenium.webdriver import Firefox
-import time
+from time import sleep
 
 
 matricula = 123
 senha = 123
+# alterado para evitar erro de linha muito longa!!
+url = "http://177.70.147.197:8080/sig/app.html#/servicosonline/portal-servidor"
 
 driver = Firefox()
-driver.get('http://177.70.147.197:8080/sig/app.html#/servicosonline/portal-servidor')
-
-time.sleep(8)
+driver.get(url)
+sleep(8)
 
 # faz o login
-driver.find_element_by_id('radio_1').click()
-driver.find_element_by_id("51inputText").send_keys(matricula)
-driver.find_element_by_id("52inputText").send_keys(senha)
+# Clica no radio para mudar para matricula!!
+driver.find_element_by_id("radio_1").click()
+sleep(1)
+# Preenche o input com a matricula
+driver.find_element_by_xpath('//*[@id="51inputText"]').send_keys(matricula)
+# Preenche o input com a senha
+driver.find_element_by_xpath('//*[@id="52inputText"]').send_keys(senha)
+# Clica em validar
 driver.find_element_by_xpath('//*[@aria-label="Validar"]').click()
 
-time.sleep(1)
+sleep(1)
 
-#clica em ponto eletrônico
+# clica em ponto eletrônico
 driver.find_element_by_xpath('//*[@heading="Ponto eletrônico"]').click()
 
-time.sleep(5)
+sleep(5)
 
-#seleciona a linha da tabela com a matrícula correta
-driver.find_element_by_xpath('//div[@class="ui-grid-viewport"]//div//div//div//div//div[@title="22397"').click()
+# seleciona a linha da tabela com a matrícula correta
+driver.find_element_by_xpath(
+    '//div[@class="ui-grid-viewport"]//div//div//div//div//div[@title="22397"'
+).click()
 
-time.sleep(5)
+sleep(5)
 
 driver.find_element_by_xpath('//*[@title="imprimir"]').click()
-
-
-
-
-
-
